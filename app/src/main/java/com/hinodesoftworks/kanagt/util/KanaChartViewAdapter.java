f67588751a778f647b4ea8c3d0b4752b660bf872
Michael Mancuso <tarkenfire@fullsail.edu>
2015-01-30 13:00:37 -0500

    emergency stubbing

---
+++ app/src/main/java/com/hinodesoftworks/kanagt/util/KanaChartViewAdapter.java
@@ -0,0 +1,55 @@
+package com.hinodesoftworks.kanagt.util;
+
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.hinodesoftworks.kanagt.R;
+
+public class KanaChartViewAdapter extends RecyclerView.Adapter<KanaChartViewAdapter.ViewHolder> {
+
+    private Cursor mCursor;
+
+    public KanaChartViewAdapter(Cursor items){
+        mCursor = items;
+    }
+
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View parent = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.chart_item_kana,
+                viewGroup, false);
+        return new ViewHolder(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int i) {
+        
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mCursor == null ? 0 : mCursor.getCount();
+    }
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView kanaDisplay;
+        public TextView romaDisplay;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            kanaDisplay = (TextView) itemView.findViewById(R.id.kana_chart_display);
+            romaDisplay = (TextView) itemView.findViewById(R.id.kana_chart_roma);
+        }
+    }
+
+
+}

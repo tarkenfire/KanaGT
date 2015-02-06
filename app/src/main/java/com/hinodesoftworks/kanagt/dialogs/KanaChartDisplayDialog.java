eb88fb4cb659b473a573d588968970d75699d3c0
Michael Mancuso <tarkenfire@fullsail.edu>
2015-02-06 13:02:25 -0500

    Added basic functionality to display fragments

--- app/src/main/java/com/hinodesoftworks/kanagt/dialogs/KanaChartDisplayDialog.java
+++ app/src/main/java/com/hinodesoftworks/kanagt/dialogs/KanaChartDisplayDialog.java
@@ -1,12 +1,16 @@
 package com.hinodesoftworks.kanagt.dialogs;
 
+import android.app.Dialog;
 import android.app.DialogFragment;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.hinodesoftworks.kanagt.R;
 
@@ -38,9 +42,87 @@ public class KanaChartDisplayDialog extends DialogFragment implements View.OnCli
         Button closeButton = (Button)parent.findViewById(R.id.kana_dialog_close_button);
         closeButton.setOnClickListener(this);
 
+        TextView kanaDisplay, romanDisplay, correctDisplay, incorrectDisplay, profDisplay;
+
+        kanaDisplay = (TextView)parent.findViewById(R.id.kana_dialog_kana_display);
+        romanDisplay = (TextView)parent.findViewById(R.id.kana_dialog_roman_display);
+        correctDisplay = (TextView)parent.findViewById(R.id.kana_dialog_correct_display);
+        incorrectDisplay = (TextView)parent.findViewById(R.id.kana_dialog_incorrect_display);
+        profDisplay = (TextView)parent.findViewById(R.id.kana_dialog_prof_display);
+
+        //set data
+        Bundle args = getArguments();
+
+        kanaDisplay.setText(args.getString("kana"));
+        romanDisplay.setText(args.getString("roman"));
+
+        StringBuilder sb = new StringBuilder();
+        int correct = args.getInt("correct", 0);
+        int incorrect = args.getInt("incorrect", 0);
+        int pCorrect = 0, pIncorrect = 0;
+
+        int total = correct + incorrect;
+
+        //prevent divide by zero exceptions
+        if (total != 0){
+            pCorrect = (correct / total) * 100;
+            pIncorrect = (incorrect / total) * 100;
+        }
+
+        sb.append("Times Correct: ");
+        sb.append(correct);
+        sb.append("(");
+        sb.append(pCorrect);
+        sb.append("%)");
+        correctDisplay.setText(sb.toString());
+
+        //reset stringbuilder
+        sb.setLength(0);
+
+        sb.append("Times Incorrect: ");
+        sb.append(incorrect);
+        sb.append("(");
+        sb.append(pIncorrect);
+        sb.append("%)");
+        incorrectDisplay.setText(sb.toString());
+
+        switch (args.getInt("prof", 0)){
+            case 0:
+                //0 is not a valid option for prof, therefore 0 == error
+                //blank the textview in this case
+                profDisplay.setText("");
+                break;
+            case 1: //unknown
+                profDisplay.setText("Unknown");
+                profDisplay.setTextColor(Color.RED);
+                break;
+            case 2: //known
+                profDisplay.setText("Known");
+                profDisplay.setTextColor(Color.YELLOW);
+                break;
+            case 3: //well known
+                profDisplay.setText("Well Known");
+                profDisplay.setTextColor(Color.BLUE);
+                break;
+            case 4: //mastered
+                profDisplay.setText("Mastered");
+                profDisplay.setTextColor(Color.GREEN);
+                break;
+        }
+
+        //todo: show stroke dialogs
+
 
 
         return parent;
+    }
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        Dialog dialog = super.onCreateDialog(savedInstanceState);
+        dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+        return dialog;
     }
 
     @Override
